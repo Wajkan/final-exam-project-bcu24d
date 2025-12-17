91e4d64f70e08e009b2f4cc7edd8dce7f4f18148
Wajkan <wiklund.karl@gmail.com>
2025-12-17 13:50:58 +0100

    getting-there
    getting-there

---
+++ front-end/src/components/WalletOptions.tsx
@@ -0,0 +1,42 @@
+import { useState, useEffect } from 'react'
+import { useConnectors } from 'wagmi'
+import type { Connector } from 'wagmi'
+import { connect } from '@wagmi/core'
+import { wagmiConfig } from '../config/wagmi'
+
+
+export function WalletOptions() {
+    
+  const connectors = useConnectors()
+  
+  return connectors.map((connector) => (
+    <WalletOption
+      key={connector.uid}
+      connector={connector}
+      onClick={() => connect(wagmiConfig, { connector })}
+    />
+  ))
+}
+
+function WalletOption({
+  connector,
+  onClick,
+}: {
+  connector: Connector
+  onClick: () => void
+}) {
+  const [ready, setReady] = useState(false)
+  
+  useEffect(() => {
+    (async () => {
+      const provider = await connector.getProvider()
+      setReady(!!provider)
+    })()
+  }, [connector])
+  
+  return (
+    <button disabled={!ready} onClick={onClick}>
+      {connector.name}
+    </button>
+  )
+}
