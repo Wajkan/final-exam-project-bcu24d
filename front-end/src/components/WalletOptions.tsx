bf1c75ec5a2ef10398fb052419d1ce979a87413d
Wajkan <wiklund.karl@gmail.com>
2025-12-18 09:21:51 +0100

    wallet-context
    wallet-context

--- front-end/src/components/WalletOptions.tsx
+++ front-end/src/components/WalletOptions.tsx
@@ -6,7 +6,7 @@ import { wagmiConfig } from '../config/wagmi'
 
 
 export function WalletOptions() {
-    
+
   const connectors = useConnectors()
   
   return connectors.map((connector) => (
@@ -16,6 +16,7 @@ export function WalletOptions() {
       onClick={() => connect(wagmiConfig, { connector })}
     />
   ))
+  
 }
 
 function WalletOption({
